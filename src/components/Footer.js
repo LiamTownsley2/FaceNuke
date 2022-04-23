0bad2befd2f281aa77eeb4a611473357d8fee07f
unknown <liamtownsley43@gmail.com>
2022-04-23 04:20:00 +0100

    feat: adding firebase & fixing security vulnerability

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -17,16 +17,16 @@ class Footer extends React.Component {
                     <div>
                         <h3>About</h3>
                         <ul>
-                            <li><a href="https://github.com/LiamTownsley/FaceNuke">The Code</a></li>
-                            <li><a href="https://github.com/LiamTownsley?tab=repositories">More of my Projects</a></li>
+                            <li><a href="https://github.com/LiamTownsley/FaceNuke" target="_blank" rel="noreferrer">The Code</a></li>
+                            <li><a href="https://github.com/LiamTownsley?tab=repositories" target="_blank" rel="noreferrer">More of my Projects</a></li>
                         </ul>
                     </div>
                     <div>
                         <h3>Contact Me</h3>
                         <ul>
-                            <li><a href="https://github.com/LiamTownsley" target="_blank">GitHub</a></li>
-                            <li><a href="https://www.linkedin.com/in/liam-townsley-0a2013237/" target="_blank">LinkedIn</a></li>
-                            <li><a href="mailto:contact@liamtownsley.me" target="_blank">E-Mail</a></li>
+                            <li><a href="https://github.com/LiamTownsley" target="_blank" rel="noreferrer">GitHub</a></li>
+                            <li><a href="https://www.linkedin.com/in/liam-townsley-0a2013237/" target="_blank" rel="noreferrer">LinkedIn</a></li>
+                            <li><a href="mailto:contact@liamtownsley.me" target="_blank" rel="noreferrer">E-Mail</a></li>
                         </ul>
                     </div>
                     <div>
--- src/index.js
+++ src/index.js
@@ -4,6 +4,22 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBy-SlyvBgcXNH_wwm-oruzHOAOP7JZG3U",
+  authDomain: "socialsite-efe60.firebaseapp.com",
+  projectId: "socialsite-efe60",
+  storageBucket: "socialsite-efe60.appspot.com",
+  messagingSenderId: "272571628583",
+  appId: "1:272571628583:web:4ba2ee259821ff01176202",
+  measurementId: "G-5VZNQVS48D"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
